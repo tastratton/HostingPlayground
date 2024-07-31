55c0e3eb707b7ed46132c9977f7b292f8b3115c5
Thomas Stratton <tastratton@georgiasown.org>
2024-07-31 13:06:41 -0500

    refactor main and fix appsettings / launchsettings

--- Program.cs
+++ Program.cs
@@ -8,45 +8,65 @@ using System.CommandLine.Parsing;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using static HostingPlayground.HostingPlaygroundLogEvents;
+using System;
+using System.Collections;
 
-namespace HostingPlayground
+namespace HostingPlayground;
+
+class Program
 {
-    class Program
+    // for dependency injection container
+    private static Func<string[], IHostBuilder> hostbuilder = args => Host.CreateDefaultBuilder(args)
+        .UseDefaultServiceProvider((context, options) => { options.ValidateScopes = true; });
+
+    private static Action<IHostBuilder> ActionConfigureHost = host =>
     {
-        static async Task Main(string[] args) => await BuildCommandLine()
-            .UseHost(_ => Host.CreateDefaultBuilder(),
-                host =>
-                {
-                    host.ConfigureServices(services =>
-                    {
-                        services.AddSingleton<IGreeter, Greeter>();
-                    });
-                })
-            .UseDefaults()
-            .Build()
-            .InvokeAsync(args);
-
-        private static CommandLineBuilder BuildCommandLine()
+        host.ConfigureServices((context, services) =>
         {
-            var root = new RootCommand(@"$ dotnet run --name 'Joe'"){
-                new Option<string>("--name"){
-                    IsRequired = true
-                }
-            };
-            root.Handler = CommandHandler.Create<GreeterOptions, IHost>(Run);
-            return new CommandLineBuilder(root);
-        }
+            services.AddSingleton<IGreeter, Greeter>();
+        });
+    };
 
-        private static void Run(GreeterOptions options, IHost host)
+    // command line
+    static async Task Main(string[] args)
+    {
+        /*
+         * Console.WriteLine("GetEnvironmentVariables: ");
+        foreach (DictionaryEntry de in Environment.GetEnvironmentVariables()) 
         {
-            var serviceProvider = host.Services;
-            var greeter = serviceProvider.GetRequiredService<IGreeter>();
-            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
-            var logger = loggerFactory.CreateLogger(typeof(Program));
-
-            var name = options.Name;
-            logger.LogInformation(GreetEvent, "Greeting was requested for: {name}", name);
-            greeter.Greet(name);
-        }
+            Console.WriteLine("  {0} = {1}", de.Key, de.Value);
+        };
+        */
+        
+        
+        await BuildCommandLine()
+        .UseHost(args => hostbuilder(args), ActionConfigureHost)
+        .UseDefaults()
+        .Build()
+        .InvokeAsync(args);
     }
+
+    private static CommandLineBuilder BuildCommandLine()
+    {
+        var root = new RootCommand(@"$ dotnet run --name 'Joe'"){
+            new Option<string>("--name"){
+                IsRequired = true
+            }
+        };
+        root.Handler = CommandHandler.Create<GreeterOptions, IHost>(Run);
+        return new CommandLineBuilder(root);
+    }
+
+    private static void Run(GreeterOptions options, IHost host)
+    {
+        var serviceProvider = host.Services;
+        var greeter = serviceProvider.GetRequiredService<IGreeter>();
+        var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger(typeof(Program));
+
+        var name = options.Name;
+        logger.LogInformation(GreetEvent, "Greeting was requested for: {name}", name);
+        greeter.Greet(name);
+    }
+
 }
